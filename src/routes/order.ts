77f8ca10a5b745333e63cdafa8a32e4a01c0c5b0
MDShihab235 <msub111244@gmail.com>
2024-02-12 11:42:00 +0600

    first commit

---
+++ src/routes/order.ts
@@ -0,0 +1,31 @@
+import express from "express";
+
+import { adminOnly } from "../middlewares/auth.js";
+import {
+  newOrder,
+  myOrders,
+  allOrders,
+  getSingleOrder,
+  processOrder,
+  deleteOrder,
+} from "../controllers/order.js";
+
+const app = express.Router();
+
+//route - /api/v1/order/new
+app.post("/new", newOrder);
+
+//route - /api/v1/order/my
+app.get("/my", myOrders);
+
+//route - /api/v1/order/all
+app.get("/all", adminOnly, allOrders);
+
+//route - /api/v1/order/dynamicId
+app
+  .route("/:id")
+  .get(getSingleOrder)
+  .put(adminOnly, processOrder)
+  .delete(adminOnly, deleteOrder);
+
+export default app;
---
+++ src/routes/product.ts
@@ -0,0 +1,31 @@
+import express from "express";
+import {
+  deleteProduct,
+  getAdminProducts,
+  getAllCategories,
+  getLatestProducts,
+  getSingleProduct,
+  newProduct,
+  searchAllProducts,
+  updateProduct,
+} from "../controllers/product.js";
+import { singleUpload } from "../middlewares/multer.js";
+import { adminOnly } from "../middlewares/auth.js";
+
+const app = express.Router();
+
+app.post("/new", adminOnly, singleUpload, newProduct);
+
+app.get("/latest", getLatestProducts);
+app.get("/all", searchAllProducts);
+
+app.get("/categories", getAllCategories);
+
+app.get("/admin-products", adminOnly, getAdminProducts);
+app
+  .route("/:id")
+  .get(getSingleProduct)
+  .put(adminOnly, singleUpload, updateProduct)
+  .delete(adminOnly, deleteProduct);
+
+export default app;
